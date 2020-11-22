9f5b175587ff4dffa1786a70abe1e40450bb372e
Jose Calucag <jose.calucag@outlook.com>
2020-11-22 05:24:52 -0500

    updating Challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -136,10 +136,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
-    if (magnitude > 5) {
+    if (magnitude > 6) {
       return "##000000";
     }
-    if (magnitude >= 4) {
+    if (magnitude >= 5) {
       return "#454545";
     }
     return "#828282";  
